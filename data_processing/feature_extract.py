50a13ab90721d9ef2b5f5a66c3c8bb447deac3e5
hptruong93 <hptruong93@gmail.com>
2016-11-21 23:13:20 -0500

    Working data processing on multiple cores

--- data_processing/feature_extract.py
+++ data_processing/feature_extract.py
@@ -1,29 +1,63 @@
 import sys
 import json
 import time
-import zlib
-
+import tarfile
 import argparse
+import textblob
 
-DATA_FILE = '../log.txt'
+DATA_FILE = '../log.tar.gz'
 
-def process_data(data):
-	# Magic happens here
-	pass
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+nprocs = comm.Get_size()
+rank = comm.Get_rank()
+
+class SentimentCounter(object):
+    def __init__(self):
+        super(SentimentCounter, self).__init__()
+
+        self.sentiment_sum = 0
+        self.sentences_count = 0
+
+    def process_data(self, data, index):
+        if nprocs == 1 or index % nprocs == rank:
+            sentiment = textblob.TextBlob(data['text']).sentiment
+            self.sentiment_sum += sentiment.polarity
+            self.sentences_count += 1
+
+    def summarize(self):
+        if rank == 0:
+            for i in xrange(1, nprocs):
+                received = comm.recv(source = i, tag = i)
+                self.sentiment_sum += received[0]
+                self.sentences_count += received[1]
+
+            print "Sum is {0} and count is {1}. On average it is {2}.".format(self.sentiment_sum, self.sentences_count, self.sentiment_sum / float(self.sentences_count))
+        else:
+            comm.send((self.sentiment_sum, self.sentences_count), dest = 0, tag = rank)
+
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description = 'Compress the logged data.')
-	parser.add_argument('-f', '--input', dest = 'input_file', default = DATA_FILE, help = 'Input data file')
-	parser.add_argument('-l', '--limit', dest = 'limit', default = 500, help = 'Limit number of lines to process. 0 to process everything.', type = int)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description = 'Compress the logged data.')
+    parser.add_argument('-f', '--input', dest = 'input_file', default = DATA_FILE, help = 'Input data file')
+    parser.add_argument('-l', '--limit', dest = 'limit', default = 500, help = 'Limit number of lines to process. 0 to process everything.', type = int)
+    args = parser.parse_args()
 
-	with open(args.input_file, 'r') as f:
-		count = 0
+    tar = tarfile.open("../log.tar.gz", "r:gz")
+    for member in tar.getmembers():
+        f = tar.extractfile(member)
+        count = 0
 
-		for line in f:
-			count += 1
-			new_data = json.loads(line)
-			process_data(new_data)
+        counter = SentimentCounter()
 
-			if count == args.limit and args.limit != 0:
-				break
+        for line in f:
+            count += 1
+
+            new_data = json.loads(line)
+            counter.process_data(new_data, count - 1)
+
+            if count == args.limit and args.limit != 0:
+                break
+
+        counter.summarize()
